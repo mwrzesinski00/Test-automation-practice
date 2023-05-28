32fee93ac59c56881a14b49e788fee6eaedf56e1
Michał Wrzesiński <m.wrzesinski00@gmail.com>
2023-05-28 14:47:22 +0200

    Implemented second test

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -9,7 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class UltimateQATest extends PageSetup {
 
 
-
     @Test
     public void testOne() {
         WebElement buttonUsingId = driver.findElementById("idExample");
@@ -17,10 +16,26 @@ public class UltimateQATest extends PageSetup {
 
         WebElement buttonSuccessText = driver.findElementByClassName("entry-title");
         assertEquals("Button success", buttonSuccessText.getText());
-
-
     }
 
+    @Test
+    public void testTwo() throws InterruptedException {
+        WebElement nameTextField = driver.findElementByName("et_pb_contact_name_0");
+        nameTextField.sendKeys("Tester");
+
+        WebElement emailTextField = driver.findElementByName("et_pb_contact_email_0");
+        emailTextField.sendKeys("random@tester.com");
+
+        Thread.sleep(2000);
+
+        WebElement emailMeButton = driver.findElementByName("et_builder_submit_button");
+        emailMeButton.click();
+
+        Thread.sleep(3000);
+
+        WebElement thanksText  = driver.findElementByClassName("et-pb-contact-message");
+        assertEquals("Thanks for contacting us", thanksText.getText());
+    }
 
 
 }
