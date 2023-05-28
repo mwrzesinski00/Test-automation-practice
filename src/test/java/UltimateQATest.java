12f7635e72eee3cac1757aa584af116ce0f1ccf2
Michał Wrzesiński <m.wrzesinski00@gmail.com>
2023-05-28 14:04:35 +0200

    Implemented first test

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -1,5 +1,9 @@
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class UltimateQATest extends PageSetup {
@@ -8,7 +12,11 @@ public class UltimateQATest extends PageSetup {
 
     @Test
     public void testOne() {
+        WebElement buttonUsingId = driver.findElementById("idExample");
+        buttonUsingId.click();
 
+        WebElement buttonSuccessText = driver.findElementByClassName("entry-title");
+        assertEquals("Button success", buttonSuccessText.getText());
 
 
     }
